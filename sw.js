4538d6f26f86cd3ddaee16a814af9b50cb45d92c
phuju <fozanmanzoor91@gmail.com>
2026-05-02 10:20:32 +0530

    Update service worker to use CACHE_NAME v2

--- sw.js
+++ sw.js
@@ -1,17 +1,18 @@
-const CACHE_NAME = "system-v1";
+const CACHE_NAME = "system-v2";
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(["./", "./index.html"]);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
